08c0329f4704bdd1079c1cde877fb8fe0cf5b46a
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-04-08 20:51:51 -0500

    feat: add avatar to chat

--- src/pages/chat/components/Chat/Chat/Content/components/Header.tsx
+++ src/pages/chat/components/Chat/Chat/Content/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@heroui/react";
+import { Avatar, Skeleton } from "@heroui/react";
 
 import { GroupInterface } from "../../../../../interfaces/Group.interface";
 import { UserInterface } from "../../../../../interfaces/User.interface";
@@ -9,21 +9,24 @@ interface Props {
   user: UserInterface | null;
 }
 
-export const Header:React.FC<Props> = ({
-  isLoading,
-  group,
-  user ,
-}) => {
+export const Header: React.FC<Props> = ({ isLoading, group, user }) => {
+  const picture = group?._id ? "https://www.svgrepo.com/show/86044/group.svg" : user?.picture
   return (
-    <div className="bg-white border-b px-4 py-2 font-semibold text-sm text-black">
+    <div className="bg-white flex items-center border-b px-4 py-2 font-semibold text-sm text-black space-x-2">
+      <Avatar
+        src={
+          picture ||
+          `https://ui-avatars.com/api/?name=${encodeURIComponent(
+            user?.name?.split(" ")[0] || "U"
+          )}&background=random&color=fff`
+        }
+      />
       {isLoading ? (
-         <Skeleton className="w-3/5 rounded-lg">
-         <div className="h-3 w-3/5 rounded-lg bg-default-200" />
-       </Skeleton>
+        <Skeleton className="w-3/5 rounded-lg">
+          <div className="h-3 w-3/5 rounded-lg bg-default-200" />
+        </Skeleton>
       ) : (
-        <span>
-          {group ? group.name : user ? user.name : "Chat"}
-        </span>
+        <span>{group ? group.name : user ? user.name : "Chat"}</span>
       )}
     </div>
   );
