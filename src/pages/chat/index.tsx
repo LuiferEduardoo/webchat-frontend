25caf0790c2c2f191a0501bf157baa74ced36258
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-04-21 23:19:47 -0500

    feat: emit sound when receiving a new message

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -11,40 +11,72 @@ import User from "./services/User";
 import Group from "./services/Group";
 
 const Chat: React.FC = () => {
-  const { socket, accessToken, updateAccessToken } = useContext(ChatContext) as ChatContextType;
+  const { socket, accessToken, updateAccessToken } = useContext(
+    ChatContext
+  ) as ChatContextType;
 
   useEffect(() => {
     const setupMessageListeners = async () => {
       try {
         const isChatInGroup = window.location.pathname.includes("chat/group");
-  
+
+        const emitSound = () => {
+          const audio = new Audio("/notification-sound.mp3");
+          audio.play().catch((error) => {
+            console.error("Error playing sound:", error);
+          });
+        };
         const handleNewGroupMessage = async (data: any) => {
-          console.log("New group message:", data);
-          const group = await Group.getGroup(accessToken, updateAccessToken, data.groupId);
-          if (!isChatInGroup && data.groupId !== window.location.pathname.split("/chat/group/").pop()?.split("/")[0]) {
+          const group = await Group.getGroup(
+            accessToken,
+            updateAccessToken,
+            data.groupId
+          );
+          if (
+            !isChatInGroup &&
+            data.groupId !==
+              window.location.pathname
+                .split("/chat/group/")
+                .pop()
+                ?.split("/")[0]
+          ) {
             addToast({
               title: `Nuevo mensaje del grupo ${group.name}`,
-              description: data.message.length > 100 ? `${data.message.substring(0, 100)}...` : data.message,
+              description:
+                data.message.length > 100
+                  ? `${data.message.substring(0, 100)}...`
+                  : data.message,
               color: "primary",
             });
+            emitSound();
           }
         };
-  
+
         const handleNewMessage = async (data: any) => {
-          console.log("New message:", data);
-          if (data.senderId !== window.location.pathname.split("/chat/").pop()?.split("/")[0]) {
-            const user = await User.getUser(accessToken, updateAccessToken, data.senderId);
+          if (
+            data.senderId !==
+            window.location.pathname.split("/chat/").pop()?.split("/")[0]
+          ) {
+            const user = await User.getUser(
+              accessToken,
+              updateAccessToken,
+              data.senderId
+            );
             addToast({
               title: `Nuevo mensaje de ${user.name}`,
-              description: data.message.length > 100 ? `${data.message.substring(0, 100)}...` : data.message,
+              description:
+                data.message.length > 100
+                  ? `${data.message.substring(0, 100)}...`
+                  : data.message,
               color: "primary",
             });
+            emitSound();
           }
         };
-  
+
         socket.on("newGroupMessage", handleNewGroupMessage);
         socket.on("newMessage", handleNewMessage);
-  
+
         // 🧹 Limpiar listeners al desmontar o antes de volver a agregar
         return () => {
           socket.off("newGroupMessage", handleNewGroupMessage);
@@ -54,10 +86,10 @@ const Chat: React.FC = () => {
         console.error("Error setting up socket listeners:", error);
       }
     };
-  
+
     setupMessageListeners();
   }, [accessToken, updateAccessToken]);
-  
+
   return (
     <div className="flex h-screen">
       <Sidebar />
